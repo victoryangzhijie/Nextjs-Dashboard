afee3d69f9bb232e84c62002aeb5478a3c0a800c
Victor Yang <victoryangzhijie@icloud.com>
2024-01-05 20:28:53 +0800

    Search & delate

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -129,9 +129,6 @@ export async function authenticate(
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-   
-    // Unreachable code block
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,18 +10,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
-      console.log(`Searching... ${term}`);
-
-      const params = new URLSearchParams(searchParams);
-      params.set('page', '1');
-      if (term) {
-        params.set('query', term);
-      } else {
-        params.delete('query');
-      }
-      replace(`${pathname}?${params.toString()}`);
-    }, 300);
+    console.log(`Searching... ${term}`);
+   
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
  
   return (
